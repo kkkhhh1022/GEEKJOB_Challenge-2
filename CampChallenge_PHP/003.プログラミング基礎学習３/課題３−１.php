6a6d3dda094505083351a91a604e3d294da4d194
宮田優真 <kabosu910@gmail.com>
2017-03-28 17:41:10 +0900

    プログラミング基礎３/課題１を提出します

---
+++ CampChallenge_PHP/003.プログラミング基礎学習３/課題３−１.php
@@ -0,0 +1,11 @@
+<?php
+    function my_profile() {
+        echo "名前: 宮田優真".'<br>';
+        echo "生年月日: 9/10/1993".'<br>';
+        echo "自己紹介: 中野区に住んでます。".'<br>';
+    }
+
+    for ($i=1; $i<=10; $i++){
+        my_profile();
+    }
+ ?>
