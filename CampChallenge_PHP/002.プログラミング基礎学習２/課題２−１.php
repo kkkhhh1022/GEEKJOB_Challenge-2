18257606b8ad92454b7efdf89236755a6b12cba5
宮田優真 <kabosu910@gmail.com>
2017-03-28 14:17:42 +0900

    プログラミング基礎２/課題１を提出します

---
+++ CampChallenge_PHP/002.プログラミング基礎学習２/課題２−１.php
@@ -0,0 +1,14 @@
+<?php
+    $num = rand(1,3);
+    switch($num) {
+        case 1:
+            echo "one".'<br>';
+            break;
+        case 2:
+            echo "two".'<br>';
+            break;
+        default:
+            echo "想定外".'<br>';
+            break;
+    }
+ ?>
