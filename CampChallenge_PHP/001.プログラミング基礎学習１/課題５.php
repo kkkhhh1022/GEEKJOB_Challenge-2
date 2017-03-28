20f5cda622fc464174a73ed1fe3382a41076337f
宮田優真 <kabosu910@gmail.com>
2017-03-28 12:43:42 +0900

    プログラミング基礎1/課題５を提出します

---
+++ CampChallenge_PHP/001.プログラミング基礎学習１/課題５.php
@@ -0,0 +1,11 @@
+<?php
+    $num = rand(0,2);
+
+    if ($num == 1) {
+        echo "1です！";
+    } elseif ($num == 2) {
+        echo "プログラミングキャンプ！";
+    } else {
+        echo "その他です！";
+    }
+ ?>
