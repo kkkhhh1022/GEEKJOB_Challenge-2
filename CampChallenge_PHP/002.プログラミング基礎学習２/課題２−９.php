4994a5cdea63959a6a205e9981000860360bf567
宮田優真 <kabosu910@gmail.com>
2017-03-28 16:21:08 +0900

    プログラミング基礎２/課題９を提出します

---
+++ CampChallenge_PHP/002.プログラミング基礎学習２/課題２−９.php
@@ -0,0 +1,9 @@
+<?php
+    $hash = array(
+                "1" => "AAA",
+                "hello" => "world",
+                "soeda" => "33",
+                "20" => "20",
+            );
+    // echo $hash["20"]; デバック用
+ ?>
