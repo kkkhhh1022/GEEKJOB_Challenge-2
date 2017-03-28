debb890417aa9be7cb0f735e3bc062b8baf1f86b
宮田優真 <kabosu910@gmail.com>
2017-03-28 17:49:13 +0900

    プログラミング基礎３/課題２を提出します

---
+++ CampChallenge_PHP/003.プログラミング基礎学習３/課題３−２.php
@@ -0,0 +1,11 @@
+<?php
+    function sort_out($a) {
+        if ($a % 2 == 0) {
+            echo "偶数".'<br>';
+        } else {
+            echo "奇数".'<br>';
+        }
+    }
+sort_out(10);
+sort_out(11);
+ ?>
