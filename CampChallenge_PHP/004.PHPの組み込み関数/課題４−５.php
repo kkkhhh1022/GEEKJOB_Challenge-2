b7baec409e4297de85be3b39146016edc9f94458
宮田優真 <kabosu910@gmail.com>
2017-03-29 20:42:26 +0900

    組み込み関数/課題５を提出します

---
+++ CampChallenge_PHP/004.PHPの組み込み関数/課題４−５.php
@@ -0,0 +1,8 @@
+<?php
+    echo strlen("宮田優真")."バイト".'<br>';
+    echo mb_strlen("宮田優真")."文字".'<br>';
+    echo strlen("みやたゆうま")."バイト".'<br>';
+    echo mb_strlen("みやたゆうま")."文字".'<br>';
+    echo strlen("miyatayuma")."バイト".'<br>';
+    echo mb_strlen("miyatayuma")."文字".'<br>';
+ ?>
