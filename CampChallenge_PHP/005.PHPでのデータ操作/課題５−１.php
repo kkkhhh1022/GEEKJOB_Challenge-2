2d3c4690442b853fab9caae470bce8a54c978ede
宮田優真 <kabosu910@gmail.com>
2017-03-30 10:55:52 +0900

    データ操作/課題１を提出します

---
+++ CampChallenge_PHP/005.PHPでのデータ操作/課題５−１.php
@@ -0,0 +1,9 @@
+<?php
+    $name = $_POST['name'];
+    $gender = $_POST['gender'];
+    $hobby = $_POST['hobby'];
+
+    echo "名前: $name<br>";
+    echo "性別: $gender<br>";
+    echo "趣味: $hobby<br>";
+ ?>
