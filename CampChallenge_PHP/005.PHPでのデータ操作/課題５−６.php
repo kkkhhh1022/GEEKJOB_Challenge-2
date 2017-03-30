cc93f4876f1bd8b47cf703a40462094894909465
宮田優真 <kabosu910@gmail.com>
2017-03-30 17:42:27 +0900

    データ操作/課題６を提出します

---
+++ CampChallenge_PHP/005.PHPでのデータ操作/課題５−６.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>課題５−６</title>
+    </head>
+    <body>
+        <h1>HTMLの練習  ←h1タグ</h1>
+        <h2>HTMLの練習  ←h2タグ</h2>
+        <h3>HTMLの練習  ←h3タグ</h3>
+        <h4>HTMLの練習  ←h4タグ</h4>
+        <h5>HTMLの練習  ←h5タグ</h5>
+        <h6>HTMLの練習  ←h6タグ</h6>
+        <hr>
+        <p><font size="1"><font color="red">HTMLの練習  ←フォントサイズ"1"</font></p>
+        <p><font size="2"><font color="blue">HTMLの練習  ←フォントサイズ"2"</font></p>
+        <p><font size="3"><font color="green">HTMLの練習  ←フォントサイズ"3"</font></p>
+        <p><font size="4"><font color="yellow">HTMLの練習  ←フォントサイズ"4"</font></p>
+        <p><font size="5"><font color="pink">HTMLの練習  ←フォントサイズ"5"</font></p>
+        <p><font size="6"><font color="purple">HTMLの練習  ←フォントサイズ"6"</font></p>
+        <p><font size="7"><font color="blown">HTMLの練習  ←フォントサイズ"7"</font></p>
+        <hr>
+        <p><font size="4"><i>HTMLの練習  ←斜体</i></font></p>
+        <p><font size="4"><strong>HTMLの練習  ←強調</strong></font></p>
+        <p><font size="4">HTMLの<ruby><rb>練習<rt>レンシュウ</ruby>  ←読み仮名</font></p>
+        <p><font size="4"><b>HTMLの練習  ←太字</b></font></p>
+    </body>
+</html>
